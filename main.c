7d4a3eab20ba35517d61fb5a8f953c400dd46c7b
Sondre <datskalf@outlook.com>
2023-11-12 14:10:55 +0000

    Update main.c

--- main.c
+++ main.c
@@ -24,10 +24,13 @@
 #include <string.h>
 #include "common.h"
 #include "maze_API.h"
+#include "output.h"
 
-int MAZE_WIDTH = 8;
-int MAZE_HEIGHT = 8;
-int RANDOM_BRANCH_LIMIT = 20;
+int mazeWidth = 8;
+int mazeHeight = 8;
+int randomBranchLimit = 20;
+int use_colours = 0;
+int print_all_branches = 0;
 
 unsigned int seed;
 
@@ -48,40 +51,108 @@ unsigned int seed;
  * @param argv An array of char* containing the arguments passed to the program.
  */
 void readParameters(int argc, char* argv[]) {
-    for (int i = 1; i < argc - 1; i++) {
-        if (strcmp(argv[i], "-w") == 0) {
-            sscanf(argv[++i], "%d", &MAZE_WIDTH);
+    char outfile[50];
+    for (int i = 1; i < argc; i++) {
+
+        // Sets the width of the maze.
+        if ((strcmp(argv[i], "-w") == 0 || strcmp(argv[i], "--width") == 0) && i+1 < argc) {
+            char readVal[10];
+            sscanf(argv[++i], "%s", readVal);
+            mazeWidth = strtol(readVal, NULL, 10);
 
             #if INCLUDE_MAX_SIZE == 1
-            if (MAZE_WIDTH >= MAZE_MAX_SIZE) {
-                MAZE_WIDTH = MAZE_MAX_SIZE;
+            if (mazeWidth >= MAZE_MAX_SIZE) {
+                mazeWidth = MAZE_MAX_SIZE;
             }
             #endif
-            #if PRINT_PARAMETER_SETUP == 1
-            printf("Set width equal to %d\n", MAZE_WIDTH);
+
+            #if PRINT_PARAMETER_SETUP >= 1
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Set width equal to %d\n", mazeWidth);
             #endif
         }
-        else if (strcmp(argv[i], "-h") == 0) {
-            sscanf(argv[++i], "%d", &MAZE_HEIGHT);
+
+
+        // Sets the height of the maze.
+        else if ((strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--height") == 0) && i+1 < argc) {
+            char readVal[10];
+            sscanf(argv[++i], "%s", readVal);
+            mazeHeight = strtol(readVal, NULL, 10);
 
             #if INCLUDE_MAX_SIZE
-            if (MAZE_HEIGHT >= MAZE_MAX_SIZE) {
-                MAZE_HEIGHT = MAZE_MAX_SIZE;
+            if (mazeHeight >= MAZE_MAX_SIZE) {
+                mazeHeight = MAZE_MAX_SIZE;
             }
             #endif
-            #if PRINT_PARAMETER_SETUP == 1
-            printf("Set height equal to %d\n", MAZE_HEIGHT);
+
+            #if PRINT_PARAMETER_SETUP >= 1
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Set height equal to %d\n", mazeHeight);
             #endif
         }
-        else if (strcmp(argv[i], "-s") == 0) {
-            sscanf(argv[++i], "%d", &seed);
 
-            #if PRINT_PARAMETER_SETUP == 1
+
+        // Sets the seed for the RNG module.
+        else if ((strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--seed") == 0) && i+1 < argc) {
+            char readVal[10];
+            sscanf(argv[++i], "%s", readVal);
+            seed = strtol(readVal, NULL, 10);
+
+            #if PRINT_PARAMETER_SETUP >= 1
+            cfprintf(stdout, GREEN, "Setup: ");
             printf("Set seed equal to %d\n", seed);
             #endif
         }
-        else if (strcmp(argv[i], "-bl") == 0) {
-            sscanf(argv[++i], "%d", &RANDOM_BRANCH_LIMIT);
+
+
+        // Sets the branch limit of the maze generator.
+        else if ((strcmp(argv[i], "-bl") == 0 || strcmp(argv[i], "--branch-limit") == 0) && i+1 < argc) {
+            char readVal[10];
+            sscanf(argv[++i], "%s", readVal);
+            randomBranchLimit = strtol(readVal, NULL, 10);
+
+            #if PRINT_PARAMETER_SETUP >= 1
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Set branch limit equal to %d\n", randomBranchLimit);
+            #endif
+        }
+
+
+        // Sets the output stream to the specified stream, or filepath if the stream is unrecognized
+        else if ((strcmp(argv[i], "-o") == 0 || strcmp(argv[i], "--output") == 0) && i+1 < argc) {
+            sscanf(argv[++i], "%s", outfile);
+
+            if (strcmp(outfile, "stdout") == 0) set_stream(stdout);
+            else if (strcmp(outfile, "stderr") == 0) set_stream(stderr);
+            else if (strcmp(outfile, "stdin") == 0) set_stream(stdin);
+            else open_file(outfile);
+
+            #if PRINT_PARAMETER_SETUP >= 1
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Set output stream to %s\n", outfile);
+            #endif
+        }
+
+
+        // Enables ANSI coloured output to the console
+        else if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--colour") == 0) {
+            use_colours = 1;
+
+            #if PRINT_PARAMETER_SETUP
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Enabled colour mode\n");
+            #endif
+        }
+
+
+        // Prints all maze branch iterations to the output file.
+        else if (strcmp(argv[i], "-pab") == 0 || strcmp(argv[i], "--print-all-branches") == 0) {
+            print_all_branches = 1;
+
+            #if PRINT_PARAMETER_SETUP
+            cfprintf(stdout, GREEN, "Setup: ");
+            printf("Printing all branch iterations\n");
+            #endif
         }
     }
 }
@@ -94,17 +165,18 @@ void readParameters(int argc, char* argv[]) {
  * @return Exit code of the program.
  */
 int main(int argc, char* argv[]) {
-    #if DEBUG_LEVEL >= 2
+#if DEBUG_LEVEL >= 1
     setvbuf(stdout, NULL, _IONBF, 0);
-    #endif
+#endif
 
+    set_stream(stdout);
     seed = time(0);
     readParameters(argc, argv);
 
     srand(seed);
     mazeInit();
     populateMaze();
-    printMaze();
+    fPrintMaze();
 
     return 0;
 }
