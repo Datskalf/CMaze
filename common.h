025531dab50c57fa82925de9768c17beea2e8fc4
Sondre <datskalf@outlook.com>
2023-11-12 14:10:42 +0000

    Update common.h

--- common.h
+++ common.h
@@ -9,15 +9,32 @@
 #ifndef MAZEGENERATOR_COMMON_H
 #define MAZEGENERATOR_COMMON_H
 
+#define PRINT_BRANCHES 2
 #define DEBUG_LEVEL 1
-#define PRINT_PARAMETER_SETUP 0
+#define PRINT_PARAMETER_SETUP 1
 #define INCLUDE_MAX_SIZE 0
-
 #define MAZE_MAX_SIZE 400
 
+#define WALL_SYMBOL "X"
+#define FREE_SYMBOL " "
+#define START_SYMBOL "S"
+#define END_SYMBOL "E"
 
-extern int MAZE_WIDTH;
-extern int MAZE_HEIGHT;
-extern int RANDOM_BRANCH_LIMIT;
+#pragma region ANSI COLOURS
+#define ANSI_COLOR_RED      "\x1b[31m"
+#define ANSI_COLOR_GREEN    "\x1b[32m"
+#define ANSI_COLOR_YELLOW   "\x1b[33m"
+#define ANSI_COLOR_BLUE     "\x1b[34m"
+#define ANSI_COLOR_MAGENTA  "\x1b[35m"
+#define ANSI_COLOR_CYAN     "\x1b[36m"
+#define ANSI_COLOR_RESET    "\x1b[0m"
+
+#pragma endregion
+
+extern int mazeWidth;
+extern int mazeHeight;
+extern int randomBranchLimit;
+extern int use_colours;
+extern int print_all_branches;
 
 #endif
